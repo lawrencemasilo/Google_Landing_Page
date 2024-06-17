2bccc1ddc89a0864148af71638264febf8f4983f
Neo Masilo <neolawrencemasilo@gmail.com>
2024-06-17 12:10:27 +0200

    corrected class to className

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
         <ul>
           <li className='links'><a href="">Gmail</a></li>
           <li className='links'><a href="">Images</a></li>
-          <li><button><span class="material-symbols-outlined">experiment</span></button></li>
-          <li><button><span class="material-symbols-outlined">apps</span></button></li>
+          <li><button><span className="material-symbols-outlined">experiment</span></button></li>
+          <li><button><span className="material-symbols-outlined">apps</span></button></li>
         </ul>
       </div>
       
@@ -20,14 +20,14 @@ function App() {
           <h1>Google</h1>
         </div>
         <div className='input'>
-          <span class="material-symbols-outlined">search</span>
+          <span className="material-symbols-outlined">search</span>
           <input type='text' placeholder='Search Google or type a URL'/>
         </div>
       </div>
 
       <div className='Add'>
         <div className='Add-icon'>
-          <span class="material-symbols-outlined">add</span>
+          <span className="material-symbols-outlined">add</span>
         </div>
         <div className='shortcut'>
           <h3>Add shortcut</h3>
@@ -35,7 +35,7 @@ function App() {
       </div>
 
       <div className='Credit'>
-        <span class="material-symbols-outlined">link</span>
+        <span className="material-symbols-outlined">link</span>
         <a href='https://unsplash.com/photos/aerial-photo-of-mountain-near-ocean-during-daytime-lHJZ2JTxSlI' target='_blank'>Photo by Pascal Debrunner</a>
       </div>
     </div>
